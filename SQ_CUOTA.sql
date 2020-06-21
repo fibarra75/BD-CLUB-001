5db6b6025430680d85ca275b6f8f3203e7c1d0ef
Victor Zepeda <vzepeda@adnet.cl>
2020-06-21 18:07:30 -0400

    Se crea secuencia cuota SQ_CUOTA

---
+++ SQ_CUOTA.sql
@@ -0,0 +1,8 @@
+--------------------------------------------------------
+-- Archivo creado  - domingo-junio-21-2020   
+--------------------------------------------------------
+--------------------------------------------------------
+--  DDL for Sequence SQ_CUOTA
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "CLUB"."SQ_CUOTA"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 81 CACHE 20 NOORDER  NOCYCLE  NOKEEP  NOSCALE  GLOBAL ;
